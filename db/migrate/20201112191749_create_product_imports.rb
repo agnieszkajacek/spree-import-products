36504347bfe8907583072d15b7661a13d93af095
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2020-11-22 19:00:26 +0100

    Add status and import errors to ProductImport

--- db/migrate/20201112191749_create_product_imports.rb
+++ db/migrate/20201112191749_create_product_imports.rb
@@ -2,6 +2,8 @@ class CreateProductImports < ActiveRecord::Migration[6.0]
   def change
     create_table :spree_product_imports do |t|
       t.string :file_name
+      t.integer :status, default: 0
+      t.string :import_errors
       t.references :user
 
       t.timestamps
