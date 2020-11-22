4a4b7af851dd0735191402500e5210d995ecdd1e
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2020-11-22 14:58:56 +0100

    Return if data file is blank

--- app/jobs/create_products_from_csv_job.rb
+++ app/jobs/create_products_from_csv_job.rb
@@ -2,7 +2,9 @@ class CreateProductsFromCsvJob < ApplicationJob
   queue_as :default
 
   def perform(product_import)
-    Imports::ImportProductsFromCsv.call(
+    return if product_import.data_file.blank?
+
+    Imports::ImportProductsFromCsvUseCase.call(
       params: {
         file: product_import.data_file
       }
