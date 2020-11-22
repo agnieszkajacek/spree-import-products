2ee72e20ba103da7006a2c290f7d111f7d91b34e
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2020-11-22 14:10:26 +0100

    Add validation

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -3,5 +3,13 @@ module Spree
   class ProductImport < ActiveRecord::Base
     has_one_attached :data_file
     belongs_to :user, class_name: Spree.user_class.to_s
+
+    validate :correct_mine_type
+
+    def correct_mine_type
+      if data_file.attached? && !data_file.content_type.in?(%w(text/csv))
+        errors.add(:data_file, 'must be a CSV file')
+      end
+    end
   end
 end
